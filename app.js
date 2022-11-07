a7d33b5bfd29f49a777e7af75c02b7b20fce43e0
Haden-Brock <stix03@gmail.com>
2022-11-07 14:21:27 -0600

    Create backend structure for project. Implement routes and controllers for getting and creating users as well as getting, creating, and deleting clothing items. Connect to Mongo using express. Create user and item schemas using Mongoose.

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const { PORT = 3001 } = process.env;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/wtwr_db');
+
+app.use((req, res, next) => {
+    req.user = {
+      _id: '63670c809dee89ca0eec5dc7'
+    };
+    next();
+});
+
+app.use('/users', require('./routes/users'));
+
+app.use('/items', require('./routes/clothingItems'));
+
+app.use((req, res, next) => {
+    const err = new Error("NotFound");
+    err.status = 404;
+    next(err);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening at port: ${PORT}`);
+});
+
+
+
---
+++ controllers/users.js
@@ -0,0 +1,41 @@
+const User = require('../models/user');
+
+module.exports.getUsers = (req, res) => {
+    User.find({})
+        .then(user => res.send({data: user}))
+        .catch(() => res.status(500).send({message: 'An error has occurred on the server.'}));
+}
+
+module.exports.getUserById = (req, res) => {
+    const userId  = req.params.userId;
+    
+    User.findById(userId)
+        .orFail(() => {
+            const err = new Error("Item ID not found");
+            err.statusCode = 404;
+            err.name = "NotFound";
+            throw err;
+        })
+        .then(user => res.send({data: user}))
+        .catch((err) => {
+            if(err.name === "NotFound" || err.name === "CastError") {
+                res.status(404).send({message: 'User ID not found.'});
+            } else {
+                res.status(500).send({message: 'An error has occurred on the server.'});
+            }
+        });
+}
+
+module.exports.createUser = (req, res) => {
+    const { name, avatar } = req.body;
+    
+    User.create({ name, avatar })
+        .then(user => res.send({data: user}))
+        .catch((err) => {
+            if(err.name === "ValidationError") {
+                res.status(400).send({message: 'Invalid data for user creation.'});
+            } else {
+                res.status(500).send({message: 'An error has occurred on the server.'});
+            }
+        });
+}
