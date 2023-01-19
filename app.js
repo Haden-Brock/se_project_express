e8671af974f1f42642ad99243cae011eac992c4d
Haden-Brock <stix03@gmail.com>
2023-01-18 21:46:24 -0600

    configure CORS

--- app.js
+++ app.js
@@ -8,7 +8,29 @@ const { validateUrl } = require('./utils/validateUrl');
 const { PORT = 3001 } = process.env;
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const app = express();
+const allowedCors = [
+  'https://hadenzbrock.students.nomoredomainssbs.ru',
+  'http://hadenzbrock.students.nomoredomainssbs.ru',
+  'localhost:3001'
+];
+const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+
 app.use(cors());
+
+app.use(function(req, res, next) {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
